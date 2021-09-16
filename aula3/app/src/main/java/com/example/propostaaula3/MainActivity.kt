b2256e491144686d4b006131c7b38ba0723d7cbd
Lucas Martins <lucasjnmartins.ljnm@gmail.com>
2021-09-16 17:11:29 -0300

    Arquivos do programa

---
+++ aula3/app/src/main/java/com/example/propostaaula3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.propostaaula3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
